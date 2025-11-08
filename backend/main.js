6a308ba34bf611ae32db08ba653600ffbacfc248
Abina91 <santhiyaabi0@gmail.com>
2025-11-08 10:00:33 +0530

    add frontend

--- backend/main.js
+++ backend/main.js
@@ -15,7 +15,7 @@ const app = express();
 mongoose.connect(process.env.MONGO_URI)
 // Middleware
 
-app.use(cors({ origin: "https://cokkistry-frontend.vercel.app/" }));
+app.use(cors({ origin: "https://cokkistry-frontend.vercel.app" }));
 app.use(express.json());
 app.use("/uploads", express.static("uploads")); // Serve uploaded images
 
@@ -238,6 +238,8 @@ app.post('/api/login', async (req, res) => {
 app.use(express.static("public"));
 
 // Start Server
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
     console.log("Server running on port 3000");
 });
